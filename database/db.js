18e02d7933cdc968f8782da14ec174299f343b3c
Felix <fguzmanceri@gmail.com>
2025-01-18 12:35:34 -0400

    updating all

--- database/db.js
+++ database/db.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CUSTOMER_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        // Connect to Customer Database
+        await mongoose.connect(process.env.MONGODB_CUSTOMER_URI);
         console.log('Connected to Customer Database');
-        await mongoose.connect(process.env.MONGODB_MAGAZINE_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        // Connect to Magazine Database
+        await mongoose.createConnection(process.env.MONGODB_MAGAZINE_URI);
         console.log('Connected to Magazine Database');
     } catch (error) {
         console.error('Database connection error:', error);
