36207b4b3bcc1b867cb7ac70e17ad2b8e5292e86
Felix <fguzmanceri@gmail.com>
2025-01-22 17:54:48 -0400

    updating config folder

--- database/db.js
+++ database/db.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
     try {
-        // Connect to Customer Database
+        // Connect to both Customer and Magazine Databases
         await mongoose.connect(process.env.MONGODB_CUSTOMER_URI);
         console.log('Connected to Customer Database');
 
-        // Connect to Magazine Database
-        await mongoose.createConnection(process.env.MONGODB_MAGAZINE_URI);
+        await mongoose.connect(process.env.MONGODB_MAGAZINE_URI);
         console.log('Connected to Magazine Database');
     } catch (error) {
         console.error('Database connection error:', error);
