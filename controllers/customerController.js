7f94a9c3bb3adc9558799f45fd33abf428ef739a
Felix <fguzmanceri@gmail.com>
2025-01-22 19:06:14 -0400

    fixing issues

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -2,11 +2,18 @@ const Customer = require('../models/Customer');
 
 exports.createCustomer = async (req, res) => {
     try {
+        const { name, email, subscriptionPlan } = req.body;
+
+        // Validate input
+        if (!name || !email || !subscriptionPlan) {
+            return res.status(400).send({ error: 'Name, email, and subscription plan are required.' });
+        }
+
         const customer = new Customer(req.body);
         await customer.save();
         res.status(201).send(customer);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send({ error: 'Server error while creating customer.' });
     }
 };
 
@@ -15,26 +22,30 @@ exports.getAllCustomers = async (req, res) => {
         const customers = await Customer.find();
         res.status(200).send(customers);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).send({ error: 'Server error while retrieving customers.' });
     }
 };
 
 exports.updateCustomer = async (req, res) => {
     try {
-        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (! customer) return res.status(404).send();
+        const { id } = req.params;
+        const customer = await Customer.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+
+        if (!customer) return res.status(404).send({ error: 'Customer not found.' });
         res.send(customer);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send({ error: 'Server error while updating customer.' });
     }
 };
 
 exports.deleteCustomer = async (req, res) => {
     try {
-        const customer = await Customer.findByIdAndDelete(req.params.id);
-        if (!customer) return res.status(404).send();
-        res.send(customer);
+        const { id } = req.params;
+        const customer = await Customer.findByIdAndDelete(id);
+
+        if (!customer) return res.status(404).send({ error: 'Customer not found.' });
+        res.status(204).send(); // No content
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).send({ error: 'Server error while deleting customer.' });
     }
 };
--- controllers/magazineController.js
+++ controllers/magazineController.js
@@ -6,7 +6,7 @@ exports.createMagazine = async (req, res) => {
         await magazine.save();
         res.status(201).send(magazine);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send({ error: 'Server error while creating magazine.' });
     }
 };
 
@@ -15,26 +15,26 @@ exports.getAllMagazines = async (req, res) => {
         const magazines = await Magazine.find();
         res.status(200).send(magazines);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).send({ error: 'Server error while retrieving magazines.' });
     }
 };
 
 exports.updateMagazine = async (req, res) => {
     try {
         const magazine = await Magazine.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!magazine) return res.status(404).send();
+        if (!magazine) return res.status(404).send({ error: 'Magazine not found.' });
         res.send(magazine);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send({ error: 'Server error while updating magazine.' });
     }
 };
 
 exports.deleteMagazine = async (req, res) => {
     try {
         const magazine = await Magazine.findByIdAndDelete(req.params.id);
-        if (!magazine) return res.status(404).send();
-        res.send(magazine);
+        if (!magazine) return res.status(404).send({ error: 'Magazine not found.' });
+        res.status(204).send(); // No content
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).send({ error: 'Server error while deleting magazine.' });
     }
 };
