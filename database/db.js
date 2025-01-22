b85085b34c26958d5426df210195c6995155ffae
Felix <fguzmanceri@gmail.com>
2025-01-22 18:05:24 -0400

    saving

--- database/db.js
+++ database/db.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const connectToDatabase = async () => {
     try {
         // Connect to the MongoDB instance
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Connected to Database');
+        await mongoose.connect(process.env.MONGODB_CUSTOMER_URI);
+        console.log('Connected to Customer Database');
+
+        await mongoose.connect(process.env.MONGODB_MAGAZINE_URI);
+        console.log('Connected to Magazine Database');
     } catch (error) {
         console.error('Database connection error:', error);
     }
