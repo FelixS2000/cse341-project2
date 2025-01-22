ee45d0bbb796bb814ef79d240245f0b17db9275f
Felix <fguzmanceri@gmail.com>
2025-01-22 18:20:01 -0400

    swagger.json changes

--- database/db.js
+++ database/db.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
     try {
-        // Connect to the MongoDB instance
+        // Connect to Customer Database
         await mongoose.connect(process.env.MONGODB_CUSTOMER_URI);
         console.log('Connected to Customer Database');
 
-        await mongoose.connect(process.env.MONGODB_MAGAZINE_URI);
+        // Connect to Magazine Database
+        await mongoose.createConnection(process.env.MONGODB_MAGAZINE_URI);
         console.log('Connected to Magazine Database');
     } catch (error) {
         console.error('Database connection error:', error);
