395e778ea2e7d9c3a93cb08de95d46a3f8ce85de
Felix <fguzmanceri@gmail.com>
2025-01-23 01:20:39 -0400

    updates routes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,9 +3,100 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 const { validateCustomer } = require('../middleware/validate');
 
+/**
+ * @swagger
+ * /customers:
+ *   post:
+ *     summary: Create a new customer
+ *     tags: [Customers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               subscriptionPlan:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Customer created successfully
+ *       400:
+ *         description: Invalid input
+ */
 router.post('/', validateCustomer, customerController.createCustomer);
+
+/**
+ * @swagger
+ * /customers:
+ *   get:
+ *     summary: Retrieve all customers
+ *     tags: [Customers]
+ *     responses:
+ *       200:
+ *         description: A list of customers
+ */
 router.get('/', customerController.getAllCustomers);
+
+/**
+ * @swagger
+ * /customers/{id}:
+ *   put:
+ *     summary: Update a customer by ID
+ *     tags: [Customers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the customer to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               subscriptionPlan:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Customer updated successfully
+ *       400:
+ *         description: Invalid input
+ *       404:
+ *         description: Customer not found
+ */
 router.put('/:id', validateCustomer, customerController.updateCustomer);
+
+/**
+ * @swagger
+ * /customers/{id}:
+ *   delete:
+ *     summary: Delete a customer by ID
+ *     tags: [Customers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the customer to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Customer deleted successfully
+ *       404:
+ *         description: Customer not found
+ */
 router.delete('/:id', customerController.deleteCustomer);
 
 module.exports = router;
--- routes/magazineRoutes.js
+++ routes/magazineRoutes.js
@@ -3,9 +3,100 @@ const router = express.Router();
 const magazineController = require('../controllers/magazineController');
 const { validateMagazine } = require('../middleware/validate');
 
+/**
+ * @swagger
+ * /magazines:
+ *   post:
+ *     summary: Create a new magazine
+ *     tags: [Magazines]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               publisher:
+ *                 type: string
+ *               genre:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Magazine created successfully
+ *       400:
+ *         description: Invalid input
+ */
 router.post('/', validateMagazine, magazineController.createMagazine);
+
+/**
+ * @swagger
+ * /magazines:
+ *   get:
+ *     summary: Retrieve all magazines
+ *     tags: [Magazines]
+ *     responses:
+ *       200:
+ *         description: A list of magazines
+ */
 router.get('/', magazineController.getAllMagazines);
+
+/**
+ * @swagger
+ * /magazines/{id}:
+ *   put:
+ *     summary: Update a magazine by ID
+ *     tags: [Magazines]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the magazine to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               publisher:
+ *                 type: string
+ *               genre:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Magazine updated successfully
+ *       400:
+ *         description: Invalid input
+ *       404:
+ *         description: Magazine not found
+ */
 router.put('/:id', validateMagazine, magazineController.updateMagazine);
+
+/**
+ * @swagger
+ * /magazines/{id}:
+ *   delete:
+ *     summary: Delete a magazine by ID
+ *     tags: [Magazines]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the magazine to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Magazine deleted successfully
+ *       404:
+ *         description: Magazine not found
+ */
 router.delete('/:id', magazineController.deleteMagazine);
 
 module.exports = router;
