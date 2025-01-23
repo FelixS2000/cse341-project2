21b04b69f32574db5145495077f287bca11cb704
Felix <fguzmanceri@gmail.com>
2025-01-22 23:19:36 -0400

    saving that

--- config/swagger.js
+++ config/swagger.js
@@ -7,17 +7,15 @@ const swaggerOptions = {
         info: {
             title: 'CRUD API',
             version: '1.0.0',
-            description: 'API for managing customers and magazines',
         },
         servers: [
             {
-                url: 'https://cse341-project2-yezt.onrender.com/', // Change this to your production URL when deployed
+                url: 'https://cse341-project2-yezt.onrender.com',
             },
         ],
     },
-    apis: ['./routes/*.js'], // Path to the API docs
+    apis: ['./routes/*.js'],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-
-module.exports = { swaggerUi, swaggerDocs };
+module.exports = swaggerDocs;
