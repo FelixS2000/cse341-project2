6bae86470d3c11aca5fbe9f63dba07d50eff161f
Felix <fguzmanceri@gmail.com>
2025-01-23 00:45:42 -0400

    update

---
+++ config/swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'CRUD API',
+            version: '1.0.0',
+            description: 'API for managing customers and magazines',
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+const swaggerUi = require('swagger-ui-express');
+
+module.exports = { swaggerDocs, swaggerUi };
