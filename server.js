8971a91695906ec9ab2881347c2a46226c32e9be
Felix <fguzmanceri@gmail.com>
2025-01-27 16:37:51 -0400

    updates

--- server.js
+++ server.js
@@ -7,7 +7,26 @@ const magazineRoutes = require('./routes/magazineRoutes');
 const passport = require('passport');
 const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 const { connectToDatabase } = require('./database/db');
+
+
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done){
+    //User.findOrCreate({ githubId: profile.id }, function (err, user){
+    return done(null, profile);    
+    //});
+    }
+));
+
+    
+
+
 
 require('dotenv').config();
 
