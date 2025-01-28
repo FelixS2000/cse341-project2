75672aebe6469bccc9edb51642f2ee6af8b50b6e
Felix <fguzmanceri@gmail.com>
2025-01-28 11:07:57 -0400

    changes

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session'); // Import express-session
 const { swaggerDocs, swaggerUi } = require('./config/swagger');
 const customerRoutes = require('./routes/customerRoutes');
 const magazineRoutes = require('./routes/magazineRoutes');
-//const passport = require('passport');
-//const session = require('express-session');
-//const GitHubStrategy = require('passport-github2').Strategy;
-//const cors = require('cors');
-//const { connectToDatabase } = require('./database/db');
-
-
+const {connectToDatabase} = require('./database/db')
 
 require('dotenv').config();
 
@@ -18,10 +13,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+
+// Set up session management
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your_secret_key', // Use a secure secret
+    resave: false,
+    saveUninitialized: true,
+}));
+
 app.use('/api/customers', customerRoutes); // Register customer routes
 app.use('/api/magazines', magazineRoutes); // Register magazine routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 
 connectToDatabase();
 
